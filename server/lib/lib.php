6b30f4e5346e0707acb28ae4d7dbb347a73b9b2b
Pranav Gandhi <hyfi@ccs.neu.edu>
2013-07-13 19:38:34 -0400

    Added a filter for url query parameters/

--- server/lib/lib.php
+++ server/lib/lib.php
@@ -11,7 +11,18 @@ function __autoload($class_name){
 function explode_url($url) {
   $raw = explode('/', $url);
   array_shift($raw);
-  return $raw;
+
+  # $raw includes stuff like home?grieag which confuses the url router, so we are moving that problem.
+  return remove_query_parameters($raw);
+}
+
+# Remove query parameters from our url. We don't need them in our routing.
+function remove_query_parameters($url_array) {
+  foreach($url_array as $key=>$val) {
+    $exploded = explode('?', $val);
+    $url_array[$key] = $exploded[0];
+  }
+  return $url_array;
 }
 
 
