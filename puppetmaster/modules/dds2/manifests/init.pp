9fa36ae659ec86e654d12b0745cc2d4523f39887
Pranav Gandhi <hyfi@ccs.neu.edu>
2013-07-14 19:35:07 +0000

    Added mysql and fixed php5-ldap

--- puppetmaster/modules/dds2/manifests/init.pp
+++ puppetmaster/modules/dds2/manifests/init.pp
@@ -1,11 +1,11 @@
 # Puppet module for DDS2
 
-class dds2-client ( {
+class dds2-client {
 
-  $packages = ['apache2', 'php5', 'php-ldap', 'imagemagick', 'mysql-server', 'php5-mysql']
-
+  $packages = ['apache2', 'php5', 'php5-ldap', 'imagemagick', 'mysql-server', 'php5-mysql']
   package { $packages: ensure => installed }
 
-  service { 'apache2': ensure => running }
+  $services = ['apache2', 'mysql']
+  service { $services: ensure => running }
 
 }
