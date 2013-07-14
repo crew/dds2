3014a52afb123388ce11935a3cc74077c04e21e1
goodwin <goodwin@dds-server.crew.ccs.neu.edu>
2013-07-13 23:41:06 -0400

    added db & db script

--- server/initial_db.sql
+++ server/initial_db.sql
@@ -27,9 +27,7 @@ Table Name: group_member
 2. ldap_uid   int
 */
 
-
-CREATE DATABASE `dds`;
-
+CREATE DATABASE IF NOT EXISTS `dds`;
 USE `dds`;
 
 -- deck
@@ -38,11 +36,18 @@ DROP TABLE IF EXISTS `deck`;
 CREATE TABLE `deck` (
   `id` int(255) NOT NULL AUTO_INCREMENT,
   `name` varchar(300) NOT NULL,
-  `data` LARGEBLOB NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_did` (`id`)
+  `data` LONGBLOB,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB;
 
+-- group_name
+
+DROP TABLE IF EXISTS `group_name`;
+CREATE TABLE `group_name` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `name` varchar(300) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB;
 
 -- deck_user
 
@@ -50,7 +55,7 @@ DROP TABLE IF EXISTS `deck_user`;
 CREATE TABLE `deck_user` (
   `did` int(255) NOT NULL,
   `uid` int(255) NOT NULL,
-  CONSTRAINT `fk_did` FOREIGN KEY (`did`) REFERENCES `deck` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+  CONSTRAINT `fk_did_deck_user` FOREIGN KEY (`did`) REFERENCES `deck` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB;
 
 -- machine_deck_assignment
@@ -59,34 +64,23 @@ DROP TABLE IF EXISTS `machine_deck_assignment`;
 CREATE TABLE `machine_deck_assignment` (
   `did` int(255) NOT NULL,
   `machine_fqdn` varchar(300) NOT NULL,
-  CONSTRAINT `fk_did` FOREIGN KEY (`did`) REFERENCES `deck` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+  CONSTRAINT `fk_did_machine_deck_assignment` FOREIGN KEY (`did`) REFERENCES `deck` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB;
 
 -- machine_deck_assignment
-
 DROP TABLE IF EXISTS `deck_group`;
 CREATE TABLE `deck_group` (
   `did` int(255) NOT NULL,
-  `gid` varchar(300) NOT NULL,
-  CONSTRAINT `fk_did` FOREIGN KEY (`did`) REFERENCES `deck` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `fk_gid` FOREIGN KEY (`gid`) REFERENCES `group_name` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB;
-
--- group_name
-
-DROP TABLE IF EXISTS `group_name`;
-CREATE TABLE `group_name` (
   `gid` int(255) NOT NULL,
-  `name` varchar(300) NOT NULL,
-   KEY `fk_gid` (`id`)
+  CONSTRAINT `fk_did_deck_group` FOREIGN KEY (`did`) REFERENCES `deck` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_gid_deck_group` FOREIGN KEY (`gid`) REFERENCES `group_name` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB;
 
 -- group_member
-
 DROP TABLE IF EXISTS `group_member`;
 CREATE TABLE `group_member` (
   `gid` int(255) NOT NULL,
   `uid` int(255) NOT NULL,
-  CONSTRAINT `fk_gid` FOREIGN KEY (`gid`) REFERENCES `group_name` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+  CONSTRAINT `fk_gid_group_member` FOREIGN KEY (`gid`) REFERENCES `group_name` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB;
 
