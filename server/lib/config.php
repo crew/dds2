5d47cfe90c66c4e69178163b03a4bf27a0ad6be6
Pranav Gandhi <hyfi@ccs.neu.edu>
2013-07-14 20:25:53 +0000

    Added PuppetDB constants in config.php

--- server/lib/config.php
+++ server/lib/config.php
@@ -11,4 +11,11 @@ define('DB_USER', '');
 define('DB_PASS', '');
 define('DB_NAME', '');
 
+# PuppetDB settings
+define('PuppetDB_LOCATION', '');
+define('PuppetDB_USER', '');
+define('PuppetDB_PASS', '');
+define('PuppetDB_NAME', '');
+
+
 ?>
