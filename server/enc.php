ae8520ff4a5c3290373e35ed3b4e1097054b8ce7
Pranav Gandhi <hyfi@ccs.neu.edu>
2013-07-15 00:38:32 +0000

    Using exit(0) now instead of return

--- server/enc.php
+++ server/enc.php
@@ -1,20 +1,14 @@
 #!/usr/bin/php
 <?php
-#set_include_path('/home/hyfi/dds2/server');
-#require 'lib/lib.php';
-#
-#$machine = Machine::find_machine($argv[1]);
-#
-#$classes = array();
-#$classes['classes']['dds-client']['slides'] = $machine->get_deck_uuids();
-#
-#echo yaml_emit($classes);
-#return 0;
-echo '---
-classes:
-  dds-client:
-    slides:
-    - 51e334b836379
-';
+set_include_path('/home/hyfi/dds2/server');
+require 'lib/lib.php';
 
-return 0;
+$machine = Machine::find_machine($argv[1]);
+
+$classes = array();
+$classes['classes']['dds-client']['slides'] = $machine->get_deck_uuids();
+
+echo yaml_emit($classes);
+exit(0);
+
+?>
