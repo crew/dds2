94c27212f4f168773d3e2f6c3032bf51536cdcdc
Pranav Gandhi <hyfi@ccs.neu.edu>
2013-07-15 01:21:03 +0000

    Removed the test slides

--- client/index.php
+++ client/index.php
@@ -5,11 +5,9 @@
   <script src="javascript/jquery-1.10.1.min.js"></script><!-- LOCAL JQUERY COPY For developing w/o internet-->
 </head>
 <body>
-
-  <div class="page-header"><h1>DDS Slide Chooser </h1></div>
   <div class="slides-window">
     <?php
-      
+
       $dirarray = preg_grep("/^([^.])/", scandir("slides")); // scan the directory, get rid if . and ..
       foreach($dirarray as &$value){ // for each file in the directory create a pane for them on the page
         $name = preg_replace("/[^A-Za-z0-9 ]/", ' ', $value);
