ae5bac877e4fc44a5d787caecea556dd3a053d76
Pranav Gandhi <hyfi@ccs.neu.edu>
2013-11-27 05:38:03 +0000

    Moved all v1.0 features to Controller form

--- server/lib/SlideController.class.php
+++ server/lib/SlideController.class.php
@@ -5,6 +5,23 @@ class SlideController extends Controller {
   public function __construct() {
     if($_SERVER['URL'][0] == 'add-slide-deck' && $this->is_post())
       $this->add_slide_deck();
+    else if($_SERVER['URL'][0] == 'assign-slides' && $this->is_post()) {
+      $this->assign_slides();
+      header('Location:/assign-slides/'.$_POST['uuid'].'?success');
+    }
+  }
+
+  private function assign_slides() {
+    # Grabs our uuid from assigned slide, and for each posted machine, assigns that slide.
+    $deck = Deck::find_deck_by_uuid($_POST['uuid']);
+
+    # This will create the definitive list of machines hosting this slide deck.
+    $deck->remove_associated_machines();
+    if(count($_POST['machines_fqdn']) > 0) {
+      foreach($_POST['machine_fqdn'] as $machine) {
+        $deck->assign_machine($machine);
+      }
+    }
   }
 
   private function add_slide_deck() {
@@ -31,6 +48,8 @@ class SlideController extends Controller {
       include 'views/add-slide-deck.php';
     else if($_SERVER['URL'][0] == 'slide-inventory')
       include 'views/slide-inventory.php';
+    else if($_SERVER['URL'][0] == 'assign-slides')
+      include 'views/assign-slide.php';
   }
 
 }
