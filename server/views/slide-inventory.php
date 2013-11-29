d47880d998b57de9708d824f53c421c9775c65aa
Pranav Gandhi <hyfi@ccs.neu.edu>
2013-11-29 21:45:06 +0000

    slides-inventory lines up perfectly now :)

--- server/views/slide-inventory.php
+++ server/views/slide-inventory.php
@@ -6,22 +6,25 @@
   </div>
 </div>
 <div class='container'>
-  <?php
+<?php
     $user = new User($_COOKIE['auth']);
     $decks = $user->user_decks;
     $i = 0;
     foreach($decks as $d) {
       if($i % 4 == 0)
-        echo "<div class='row'>\n  "; ?>
-  <div class='col-xs-3 slidedeck'>
-      <a href="/assign-slides/<?=$d->get_uuid()?>/"><img src="slides/<?=$d->get_uuid()?>/slide.jpg">
-      <h3 class='text-center'><?=$d->get_name()?></h3>
+        echo "  <div class='row'>\n";
+      echo <<<SLIDE
+    <div class='col-sm-3 slidedeck'>
+      <a href="/assign-slides/{$d->get_uuid()}/">
+        <img src="slides/{$d->get_uuid()}/slide.jpg">
+        <h3 class='text-center'>{$d->get_name()}</h3>
       </a>
-    </div>
-  <?  if($i % 4 == 3)
-        echo "</div>\n";
+    </div>\n
+SLIDE;
+      if($i % 4 == 3)
+        echo "  </div>\n";
       $i++;
-    } ?>
-
+    }
+?>
 </div>
 <?php include 'footer.php'?>
