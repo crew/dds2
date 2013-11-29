4576673dac6d5792d540a0329eb747c1163b0db1
Pranav Gandhi <hyfi@ccs.neu.edu>
2013-11-29 05:30:56 +0000

    Added the new navbar, but everything else is broken.

--- server/views/header.php
+++ server/views/header.php
@@ -5,24 +5,36 @@
 <html>
   <head>
     <title>Digital Display System</title>
-    <link rel="stylesheet" type="text/css" href="/bootstrap/css/bootstrap.css" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.2/css/bootstrap.min.css">
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.2/js/bootstrap.min.js"></script>
     <link rel="stylesheet" type="text/css" href="/css/main.css" />
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
   </head>
   <body>
-    <div class='container'>
-      <div class="navbar">
-        <div class="navbar-inner">
-          <a class="brand" href="/slide-inventory">Digital Display System</a>
-          <ul class="nav">
-            <? if(Login::is_user_logged_in()) { ?>
-              <li><a href="/add-slide-deck">Add Slide Deck</a></li>
-              <li><a href="/slide-inventory">Slide Deck Inventory</a></li>
-              <li><a href="/logout">Logout</a></li>
-            <? } else { ?>
-              <li><a href="/login">Login</a></li>
-            <? } ?>
-          </ul>
-        </div>
-      </div>
-    </div>
+    <nav class="navbar navbar-default" role="navigation">
+      <div class='container'>
+       <!-- Brand and toggle get grouped for better mobile display -->
+       <div class="navbar-header">
+         <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+           <span class="sr-only">Toggle navigation</span>
+           <span class="icon-bar"></span>
+           <span class="icon-bar"></span>
+           <span class="icon-bar"></span>
+         </button>
+         <a class="navbar-brand" href="#">Digital Display System</a>
+       </div>
+       <!-- Collect the nav links, forms, and other content for toggling -->
+       <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+         <ul class="nav navbar-nav">
+           <? if(Login::is_user_logged_in()) { ?>
+           <li><a href="/add-slide-deck">Add Slide Deck</a></li>
+           <li><a href="/slide-inventory">Slide Deck Inventory</a></li>
+           <li><a href="/logout">Logout</a></li>
+           <? } else { ?>
+           <li><a href="/login">Login</a></li>
+           <? } ?>
+         </ul>
+       </div>
+     </div>
+   </nav>
