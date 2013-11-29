5034883c6ce583b4816f939a004f444015402ccd
Pranav Gandhi <hyfi@ccs.neu.edu>
2013-11-29 05:20:04 +0000

    Better styling on slide-inventory but bootstrap3 is going to be required.

--- server/views/header.php
+++ server/views/header.php
@@ -10,18 +10,19 @@
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
   </head>
   <body>
-    <div class="navbar">
-      <div class="navbar-inner">
-        <a class="brand" href="/slide-inventory">Digital Display System</a>
-        <ul class="nav">
-          <? if(Login::is_user_logged_in()) { ?>
-            <li><a href="/add-slide-deck">Add Slide Deck</a></li>
-            <li><a href="/slide-inventory">Slide Deck Inventory</a></li>
-            <li><a href="/logout">Logout</a></li>
-          <? } else { ?>
-            <li><a href="/login">Login</a></li>
-          <? } ?>
-        </ul>
+    <div class='container'>
+      <div class="navbar">
+        <div class="navbar-inner">
+          <a class="brand" href="/slide-inventory">Digital Display System</a>
+          <ul class="nav">
+            <? if(Login::is_user_logged_in()) { ?>
+              <li><a href="/add-slide-deck">Add Slide Deck</a></li>
+              <li><a href="/slide-inventory">Slide Deck Inventory</a></li>
+              <li><a href="/logout">Logout</a></li>
+            <? } else { ?>
+              <li><a href="/login">Login</a></li>
+            <? } ?>
+          </ul>
+        </div>
       </div>
     </div>
-
--- server/views/slide-inventory.php
+++ server/views/slide-inventory.php
@@ -1,20 +1,21 @@
 <?php include 'header.php'?>
 <div class='container'>
   <div class='row'>
-    <div class='span4'>
-      <h1>Your Slides</h1>
+    <div class='span12'>
+      <h1>Your Slide Decks</h1>
     </div>
   </div>
-  <div>
+  <div class="row">
     <?php
       $user = new User($_COOKIE['auth']);
       $decks = $user->user_decks;
+      $i = 1;
       foreach($decks as $d) { ?>
-        <div class='span3'>
+        <div class='span3 slide'>
           <a href="/assign-slides/<?=$d->get_uuid()?>/"><img src="slides/<?=$d->get_uuid()?>/slide.jpg">
           <h3 class='text-center'><?=$d->get_name()?></h3></a>
         </div>
-     <? } ?>
+      <? } ?>
   </div>
 </div>
 <?php include 'footer.php'?>
